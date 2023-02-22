358a97a3528c68e55e5fadb808ce4cda99fe7ae3
MurilloNS <murillo.murillo2001@gmail.com>
2023-02-22 20:38:30 -0300

    Creating Vagas Microservice

---
+++ ms-vaga/src/test/java/com/vagasproject/msvaga/MsVagaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vagasproject.msvaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsVagaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
